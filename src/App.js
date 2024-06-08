4224505e8c97ef0e01d9762b92a05531cce9f909
johnnykhaw <johnnykhaw5633@gmail.com>
2024-06-08 11:31:11 +0800

    make it better

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to UKMTube</h2>
+      <video width="700px" height="400px" controls>
+        <source src="https://d1qy0rlkkmxkst.cloudfront.net/Universiti%20Kebangsaan%20Malaysia%20(UKM),%20Bangi%20Selangor%20(4k%20Video).mp4" type="video/mp4"/>
+      </video>
+    </div>
+  );
+}
+
+export default App;
